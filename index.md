6131eddd4df196bf918b93688ceef283c76520a0
David Bruant <davidbruant@protonmail.com>
2021-05-08 22:23:14 +0200

    Plutôt 2019

--- index.md
+++ index.md
@@ -6,7 +6,7 @@ styles:
     - href: "./build/bundle.css"
 ---
 
-# Données Gironde 2020
+# Données Gironde 2019
 
 Lignes budgétaires par ordre décroissant pour atteindre 95% du montant total de chaqu catégorie
 
--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,14 @@ json('./data/gironde-finance-data.json')
 .then(data => {
 	console.log('data', data)
 
-	const data2020 = data.aggregations.find(agg => agg.year === 2020).aggregation.children
+	const data2019 = data.aggregations.find(agg => agg.year === 2019).aggregation.children
 
-	console.log('data2020', data2020)
+	console.log('data2019', data2019)
 
 	const app = new App({
 		target: document.querySelector('.svelte-main'),
 		props: {
-			groupes: data2020
+			groupes: data2019
 		}
 	});
 })
